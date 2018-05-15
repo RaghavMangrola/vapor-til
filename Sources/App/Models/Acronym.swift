4a75258a7f4b2618d604ca85e1c582b98dea9325
Raghav Mangrola <raghav.mangrola@eventfarm.com>
2018-05-14 21:14:33 -0700

    Add parent-child relationship

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -30,17 +30,33 @@ import FluentPostgreSQL
 import Vapor
 
 final class Acronym: Codable {
-  var id: UUID?
-  var short: String
-  var long: String
+    var id: UUID?
+    var short: String
+    var long: String
+    var userID: User.ID
 
-  init(short: String, long: String) {
-    self.short = short
-    self.long = long
-  }
+    init(short: String, long: String, userID: User.ID) {
+        self.short = short
+        self.long = long
+        self.userID = userID
+    }
+}
+
+extension Acronym {
+    var user: Parent<Acronym, User> {
+        return parent(\.userID)
+    }
 }
 
 extension Acronym: PostgreSQLUUIDModel{}
 extension Acronym: Content {}
-extension Acronym: Migration {}
 extension Acronym: Parameter {}
+
+extension Acronym: Migration {
+    static func prepare(on connection: PostgreSQLConnection) -> Future<Void> {
+        return Database.create(self, on: connection) { builder in
+            try addProperties(to: builder)
+            try builder.addReference(from: \.userID, to: \User.id)
+        }
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -36,4 +36,7 @@ public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
     let acronymsController = AcronymsController()
     try router.register(collection: acronymsController)
+
+    let usersController = UsersController()
+    try router.register(collection: usersController)
 }
